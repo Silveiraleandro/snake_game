b272c8358d0b6fa2ec602a98ea6403b1e2fd2ff0
Leandro Silveira <leovoz@yahoo.com.br>
2025-08-01 17:58:00 -0300

    Adding a window and score

--- view/main.py
+++ view/main.py
@@ -27,3 +27,16 @@ def check_collisions():
 
 def game_over():
     pass
+
+window = Tk()
+window.title("Snake Game")
+window.resizable(False, False)
+
+score = 0
+direction = 'down'
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+
+window.mainloop()
