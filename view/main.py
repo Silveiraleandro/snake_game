29c7eb1f1a9dad7514294426b261172579c73415
Leandro Silveira <leovoz@yahoo.com.br>
2025-08-01 18:33:39 -0300

    centralizing the window

--- view/main.py
+++ view/main.py
@@ -34,11 +34,27 @@ window.resizable(False, False)
 
 score = 0
 direction = 'down'
-
+# setting up the window
 label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
 label.pack()
 
+# creating the game board
 canvas = Canvas(window, background=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
+
+# window centralization
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+# adjust the position of the window
+# casting the values to integer, so they can be used it in the geometry
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+# setting the geometry
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 window.mainloop()
